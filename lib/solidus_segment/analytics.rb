3c73cebd7eda36c2a5000173f35622dd23069dd3
Alessio Rocco <alessio.rocco.lt@gmail.com>
2020-04-20 15:16:12 +0200

    Allow identifying a user through analytics backend

--- lib/solidus_segment/analytics.rb
+++ lib/solidus_segment/analytics.rb
@@ -4,8 +4,27 @@ module SolidusSegment
   class Analytics
     attr_reader :backend
 
-    def initialize(backend: SolidusSegment.configuration.segment_backend)
+    def initialize(user: nil,
+                   anonymous_id: nil,
+                   backend: SolidusSegment.configuration.segment_backend)
+      @user = user
+      @anonymous_id = anonymous_id
       @backend = backend
+    end
+
+    def identify
+      backend.identify(identify_params)
+    end
+
+    private
+
+    attr_reader :user, :anonymous_id
+
+    def identify_params
+      {
+        user_id: user&.id,
+        anonymous_id: anonymous_id,
+      }
     end
   end
 end
--- spec/lib/solidus_segment/analytics_spec.rb
+++ spec/lib/solidus_segment/analytics_spec.rb
@@ -3,6 +3,8 @@
 require "spec_helper"
 
 RSpec.describe SolidusSegment::Analytics do
+  let(:user) { build_stubbed(:user) }
+
   before do
     SolidusSegment.configuration.segment_write_key = 123
     SolidusSegment.configuration.segment_backend = nil
@@ -18,5 +20,30 @@ RSpec.describe SolidusSegment::Analytics do
     backend = instance_double("Backend::Analytics")
 
     expect(described_class.new(backend: backend).backend).to eq(backend)
+  end
+
+  describe "#identify" do
+    context 'when the defaul backend is used' do
+      it "raises an error if user and anonymous_id aren't given" do
+        expect{ described_class.new.identify }.to raise_error(ArgumentError)
+      end
+
+      it "doesn't raise an error if only the user is given" do
+        expect{ described_class.new(user: user).identify }.not_to raise_error
+      end
+
+      it "doesn't raise an error if only the anonymous_id is given" do
+        expect{ described_class.new(anonymous_id: 'abc').identify }.not_to raise_error
+      end
+    end
+
+    it "identifies the user by id and anonymous_id" do
+      backend = instance_double('Backend::Analytics')
+      allow(backend).to receive(:identify)
+
+      described_class.new(user: user, anonymous_id: 'abc', backend: backend).identify
+
+      expect(backend).to have_received(:identify).with(user_id: user.id, anonymous_id: 'abc')
+    end
   end
 end
